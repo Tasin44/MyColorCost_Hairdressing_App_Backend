80aa6fccd4cce62d9d3b117c17efd7a2bb7667ff
root <root@srv1245656.hstgr.cloud>
2026-03-18 06:20:22 +0000

    server success code 18-march-2026

--- myproject/settings.py
+++ myproject/settings.py
@@ -97,13 +97,23 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES : Dict[str, Dict[str, Any]] = {
+# DATABASES : Dict[str, Dict[str, Any]] = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'color_grase_db'),  # this must match POSTGRES_DB
+        'USER': os.environ.get('DB_USER', 'lazy_alien'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'aTm8/UyJSFBN'),
+        'HOST': os.environ.get('DB_HOST', 'db'),  # must be 'db', not 'localhost'
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
