abd690076d97305e5496ddc59e3dbce8951ce8ef
root <root@srv1245656.hstgr.cloud>
2026-03-18 05:44:18 +0000

    server success code 18 march-2026

--- myproject/settings.py
+++ myproject/settings.py
@@ -92,10 +92,21 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES : Dict[str, Dict[str, Any]] = {
+# DATABASES : Dict[str, Dict[str, Any]] = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'color_grase_db'),
+        'USER': os.environ.get('DB_USER', 'lazy_alien'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'aTm8/UyJSFBN'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
